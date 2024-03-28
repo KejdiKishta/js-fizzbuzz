03669d7cfad4cb68d44fccd6152862949ca7ce27
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-03-28 10:42:54 +0100

    fix: commented the elements that were not nedeed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 //* seleziono il container in cui poi voglio andare a mettere i quadrati numerati
 const containerElem = document.querySelector(".container"); //object | null
 
@@ -10,28 +12,21 @@ for (let i = 1; i <101; i++) {
     boxElem.classList.add("box");
     console.log(boxElem);
 
-    //? condizione colori
-    if (i % 3 === 0) {
-    } else if ((i+1) % 3 === 0) {
-    } else {
-    }
-
     if (i % 3 === 0 && i % 5 === 0) {
         boxElem.innerHTML = `<img src="./Screenshot 2024-03-27 170956.png" alt="">`
-        boxElem.classList.add("fizzbuzz")
+        // boxElem.classList.add("fizzbuzz")
         boxElem.classList.add("color3")
     } else if (i % 5 === 0) {
         boxElem.innerHTML = `<img src="./Immagine 2024-03-27 170823.png" alt="">`
-        boxElem.classList.add("fizzbuzz")
+        // boxElem.classList.add("fizzbuzz")
         boxElem.classList.add("color2")
     } else if  (i % 3 === 0){
         boxElem.innerHTML = `<img src="./Screenshot 2024-03-27 170905.png" alt="">`
-        boxElem.classList.add("fizzbuzz")
+        // boxElem.classList.add("fizzbuzz")
         boxElem.classList.add("color1")
     } else {
         boxElem.classList.add("color4")
     }
-
 
     containerElem.append(boxElem)
 }
